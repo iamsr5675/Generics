dba461961327865108f4925a663c59b88c855491
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-10 01:37:37 +0530

    Find max of float

--- src/main/java/com/bridgelabz/testmaximumgenerics/TestMaximum.java
+++ src/main/java/com/bridgelabz/testmaximumgenerics/TestMaximum.java
@@ -2,7 +2,7 @@ package com.bridgelabz.testmaximumgenerics;
 
 public class TestMaximum
 {
-	public static int Findmax(Integer[] intArray) {
+	public static int Findmax(Integer[] intArray){
         int max = intArray[0];
         if(intArray[1].compareTo(max)>0)
         {
@@ -14,10 +14,30 @@ public class TestMaximum
         }
         return max;
     }
-	
+
+    public static float Findmax1(Float[] floatArray){
+        float max = floatArray[0];
+        if(floatArray[1].compareTo(max)>0)
+        {
+            max = floatArray[1];
+        }
+        if (floatArray[2].compareTo(max)>0)
+        {
+            max = floatArray[2];
+        }
+        return max;
+
+    }
+
     public static void main(String[] args) {
-        Integer[] intArray = {3,7,1};
+        Integer[] intArray = {4,7,1};
+        Float[] floatArray = {6.5f,2.0f,9.2f};
+
+
         int maxnumint = Findmax(intArray);
-        System.out.println("Max Integer: "+maxnumint);
+        System.out.println("Max Integer is: "+maxnumint);
+
+        float maxnumfloat = Findmax1(floatArray);
+        System.out.println("Max Float is: "+maxnumfloat);
     }
 }
